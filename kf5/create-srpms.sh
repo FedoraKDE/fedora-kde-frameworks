4b1e27934b38e0935f24191e59daaebe0db67f2c
Dan Vrátil <dvratil@redhat.com>
2014-01-09 19:16:27 +0100

    Kill the overly-optimistic and naive shell scripts
    Kids: don't drink and write bash scripts

--- kf5/create-srpms.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for tier in tier1 tier2 tier3 tier4; do
-        frameworks=`ls ${tier}`
-        for fw in ${frameworks}; do
-                rpmbuild -bs ${tier}/${fw}
-        done
-done
